3059c3f80ba61afe18101da1436724ff56a24e66
Oliver Heward <oliverhewardbie@gmail.com>
2019-01-18 17:05:42 +0000

    Fixing Bugs, Adding Content

--- smoothscroll.js
+++ smoothscroll.js
@@ -1,3 +1,6 @@
+
+// $(document).ready(function {
+
 jQuery(document).ready(function($) {
   function scrollToSection(event) {
     event.preventDefault();
---
+++ test.js
@@ -0,0 +1,12 @@
+const $fontSize = $("p").css(
+	{ "font-size": "0.875rem",
+	"line-height" : "2"
+});
+
+console.log($fontSize);
+
+const $h2Size = $("h2").css(
+	{ "font-size": "2rem"
+});
+
+
