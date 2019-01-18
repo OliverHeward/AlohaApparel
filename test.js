56c3bd1c507e373b0c436c3d191af8265a332581
Oliver Heward <oliverhewardbie@gmail.com>
2019-01-18 19:01:00 +0000

    Final Commit This Week

--- test.js
+++ test.js
@@ -1,12 +1,30 @@
-const $fontSize = $("p").css(
+// Paragraph Font Size jQuery Over-Ride
+const $pFonts = $("p").css(
 	{ "font-size": "0.875rem",
-	"line-height" : "2"
+	"line-height" : "2",
+	"font-family": "Raleway-Light",
+	"line-height": "2rem",
+	"padding": "10px",
+	"text-align" : "center"
+
 });
 
-console.log($fontSize);
+// Headline 1 fonts jQuery Over-Ride
+const $h1Fonts = $("h1").css(
+	{"font-size": "3rem",
+	"font-family": "PlayfairDisplay-Regular",
 
-const $h2Size = $("h2").css(
-	{ "font-size": "2rem"
 });
 
+// Headline 2 Fonts jQuery Over-Ride
+const $h2Fonts = $("h2").css(
+	{ "font-size": "2rem",
+	"text-align" : "center",
+});
 
+// Headline 3 Fonts jQuery Over-Ride
+const $h3Fonts = $("h3").css(
+	{"font-size" : "1.25rem",
+	"text-transform" : "uppercase",
+
+});
