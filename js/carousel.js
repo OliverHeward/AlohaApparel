df509f1b2e1a282c1ce31804ee422d35418571d7
Oliver Heward <oliverhewardbie@gmail.com>
2019-02-02 21:51:56 +0000

    jQuery add to cart increment added, Everything complete :)

--- js/carousel.js
+++ js/carousel.js
@@ -1,16 +1,29 @@
-(function($){
-	
-	$(document).ready(function(){
+(function($) {
 
-		$('.main-carousel').flickity({
-			freeScroll: true,
-			cellAlign: '50px',
-			// wrapAround: true,
-			groupCells: 2,
-			contain: true,
-		});
+    $(document).ready(function() {
 
-	});
+        $('.main-carousel').flickity({
+            freeScroll: true,
+            cellAlign: '50px',
+            // wrapAround: true,
+            groupCells: 2,
+            contain: true,
+        });
+
+    });
 
 
 })(jQuery)
+
+var counter = 0;
+
+$(function() {
+
+    $(".buy-button").click(function() {
+        counter++;
+
+        $("#cart-counter").text(counter);
+        $(this).text('Added To Basket').css('background', '#e2574c');
+    });
+
+});
--- js/subscribe.js
+++ js/subscribe.js
@@ -1,6 +1,7 @@
-$(function(){
-$(".subscribe").on("click", function() {
-	$(".subscribe").text("Clicked"),
-	alert("Thanks for subscribing!");
-})
+$(function() {
+    $(".subscribe").on("click", function() {
+        $(".subscribe").text("Clicked"),
+            var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        alert("Thanks for subscribing!");
+    })
 });
