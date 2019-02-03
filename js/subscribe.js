0e0198ad4000347f6d24fca5a53af70861847552
Oliver Heward <oliverhewardbie@gmail.com>
2019-02-03 21:28:14 +0000

    Improved browser compatibility & Aloha scroll too home

--- js/subscribe.js
+++ js/subscribe.js
@@ -2,8 +2,8 @@ var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
 $(function() {
     $(".subscribe").on("click", function() {
+    	$(this).val() = regex,
         $(".subscribe").text("Clicked"),
-        $(this).val() = regex,
         alert("Thanks for subscribing!");
     })
 });
